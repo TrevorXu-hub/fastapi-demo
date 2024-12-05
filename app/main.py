e755886e2095c5d5454ca171f57cfdd701fdc3b8
XZL <xzl@XZLdeMacBook-Pro.local>
2024-12-05 10:26:40 -0500

    fastapi update

--- app/main.py
+++ app/main.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import json
+from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+from mysql.connector import Error
 import os
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello fuck"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "pxg6af"  # Replace with your actual database name
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-	return{"product": c * d}
+try:
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur = db.cursor()
+except Error as e:
+    print("Error connecting to database:", e)
 
-@app.get("/square/{c}")
-def square(c: int):
-        return{"product": c * c}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
-
+# 定义 /songs 路由
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, songs.genre
+    FROM songs
+    JOIN genres ON songs.genre = genres.genreid
+    ORDER BY songs.id;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
